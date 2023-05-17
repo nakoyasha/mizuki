0909b3dd153f3c6a7eca8ebd2d1984ea95e22225
Haruka <96925398+nakoyasha@users.noreply.github.com>
2023-05-17 09:45:12 +0300

    fix: accidentally commited the bot token
    oops

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import * as dotenv from "dotenv"
+dotenv.config()
+
 import { client } from "./ClientContainer"
 import Listeners from "./Listeners/Listeners"
+
 
 client.on("ready", Listeners.Ready)
 client.on("interactionCreate", Listeners.InteractionCreate)
 
-client.login("MTEwODEyNzE4NzgyMDg3MTc5MA.GPQ2w6.m8tbdPTHs2-rxrmcx1LE0jm4AYY6aYTrt65F4g")
+client.login(process.env.TOKEN)
