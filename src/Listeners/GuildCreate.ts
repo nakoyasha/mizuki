65d19f1477c34db4431c03035571e0f0e1bc8ee7
Haruka <96925398+nakoyasha@users.noreply.github.com>
2023-12-23 21:12:37 +0200

    Cleanup imports in GuildCreate

--- src/Listeners/GuildCreate.ts
+++ src/Listeners/GuildCreate.ts
@@ -1,8 +1,7 @@
 import { Guild } from "discord.js";
-import Logger from "../System/Logger";
+import Logger from "@system/Logger"
 import { DatabaseSystem } from "@system/Database/DatabaseSystem";
 const GuildCreateLogger = new Logger("GuildCreate")
-
 
 export default async (Guild: Guild): Promise<void> => {
     await initializeServerData(Guild)
