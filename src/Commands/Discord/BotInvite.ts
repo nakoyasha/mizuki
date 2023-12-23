474dc7c289b0a419fd1adefc0819dad6b77e2591
Haruka <96925398+nakoyasha@users.noreply.github.com>
2023-12-23 11:31:06 +0200

    Add BotInvite Command

---
+++ src/Commands/Discord/BotInvite.ts
@@ -0,0 +1,15 @@
+import { CommandInteraction, ApplicationCommandOptionType, ApplicationCommandType } from "discord.js";
+import { Command } from "src/CommandInterface";
+
+export const BotInvite: Command = {
+    name: "invite",
+    options: [
+    ],
+    description: "Sends a bot invite.",
+    type: ApplicationCommandType.ChatInput,
+    deferReply: false,
+    ownerOnly: true,
+    run: async (interaction: CommandInteraction) => {
+        interaction.reply({ content: "https://discord.com/api/oauth2/authorize?client_id=1108127187820871790&permissions=139586732118&scope=bot", ephemeral: true })
+    }
+};
