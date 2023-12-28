749e0cffa5a4a4d3316bbe060cc8819abb19ab64
Haruka <96925398+nakoyasha@users.noreply.github.com>
2023-12-29 00:59:30 +0200

    DM the user about their ban, and add an unban user message button

--- src/Commands/Moderation/ban.ts
+++ src/Commands/Moderation/ban.ts
@@ -1,8 +1,7 @@
-import { ApplicationCommandOptionType, CommandInteraction, Emoji, GuildEmoji, GuildMember, PermissionsBitField, TextBasedChannel } from "discord.js";
+import { ButtonBuilder, ApplicationCommandOptionType, CommandInteraction, Emoji, GuildEmoji, GuildMember, PermissionsBitField, TextBasedChannel, ButtonStyle, ActionRowBuilder, ButtonComponent } from "discord.js";
 import { Command } from "../../CommandInterface";
 import { channels } from "../../Maps/ChannelsMap";
 import { EmbedBuilder } from "@discordjs/builders";
-import Emojis from "../../Maps/EmojisMap";
 import MakeErrorEmbed from "../../Util/MakeErrorEmbed";
 
 export const ban: Command = {
@@ -22,14 +21,20 @@ export const ban: Command = {
             required: false,
         },
         {
-            name: "deletemessagehistory",
+            name: "deleteMessageHistory",
             description: "Delete their message history?",
             type: ApplicationCommandOptionType.Number,
             required: false,
         },
         {
-            name: "withappeal",
+            name: "withAppeal",
             description: "Should they be able to submit an appeal? (defauls to false)",
+            type: ApplicationCommandOptionType.Boolean,
+            required: false,
+        },
+        {
+            name: "quiet",
+            description: "If enabled, the offender will not receive a DM from the bot telling them about their ban.",
             type: ApplicationCommandOptionType.Boolean,
             required: false,
         }
@@ -42,7 +47,9 @@ export const ban: Command = {
         const deleteMessageHistory = interaction.options.get("deleteMessageHistory")?.value as number
         const offender = interaction.options.getUser("user")
         const offenderMember = interaction.guild?.members.cache.get(offender?.id as string) as GuildMember
-        const reason = interaction.options.get("reason")?.value as string || "No reason specified"
+        const banReason = interaction.options.get("reason")?.value as string || "No reason specified"
+        const shouldDM = interaction.options.get("quiet")?.value as boolean
+
 
         if (moderator == offender) {
             await interaction.reply("Are you stupid?");
@@ -50,9 +57,12 @@ export const ban: Command = {
         }
 
         try {
+            if (shouldDM == true) {
+                await offenderMember.send(`You have been banned from ${interaction.guild?.name} by ${moderator.username} for ${banReason}`)
+            }
             await offenderMember.ban({
                 deleteMessageSeconds: deleteMessageHistory || 0,
-                reason: reason
+                reason: banReason
             })
 
             const embed = new EmbedBuilder()
@@ -60,10 +70,24 @@ export const ban: Command = {
                     text: moderator.username,
                     iconURL: moderator.avatarURL() as string,
                 })
-                .setDescription(`${offender?.username} was banned by ${moderator.username} for ${reason}`)
+                .setDescription(`${offender?.username} was banned by ${moderator.username} for ${banReason}`)
 
             const message = await channel.send({ embeds: [embed] });
             message.react("💀")
+
+            const unbanButton = new ButtonBuilder()
+                .setCustomId(`unban${offender?.id}`)
+                .setLabel("Unban user")
+                .setStyle(ButtonStyle.Danger)
+
+            const row = new ActionRowBuilder<ButtonBuilder>()
+                .addComponents(unbanButton);
+
+            await interaction.reply({
+                content: `Successfully banned ${offender?.username}. They have been DM'd about the ban.`,
+                components: [row],
+                ephemeral: true,
+            })
         } catch (error) {
             const embed = MakeErrorEmbed(`Failed to ban user: ${error}`)
             await interaction.reply({ embeds: [embed] })
