c953bc332ffa5e6da2d69c84bd0281f70a9e498c
Haruka <96925398+nakoyasha@users.noreply.github.com>
2023-12-28 23:09:00 +0200

    SRS: Fix the CDN url

--- src/resolveSRSAsset.ts
+++ src/resolveSRSAsset.ts
@@ -1,3 +1,3 @@
 export default function resolve_srs_asset(asset: any) {
-    return "https://starrailstation.com/assets/" + asset + ".webp"
+    return "https://cdn.starrailstation.com/assets/" + asset + ".webp"
 }
