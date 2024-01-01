ffd7e760496dd215d8c32507573861fa241f9dd1
Sebastian Erik Bauer <dataisloading@outlook.com>
2024-01-01 18:45:34 +0100

    Add .eslint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Mizuki } from "@system/Mizuki"
-import * as dotenv from "dotenv"
-dotenv.config()
+import { Mizuki } from "@system/Mizuki";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 Mizuki.init();
 Mizuki.start();
