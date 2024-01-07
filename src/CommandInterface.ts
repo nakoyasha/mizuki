e061d0576a4f39fd686e1036e6782c90f409fab5
Haruka <96925398+nakoyasha@users.noreply.github.com>
2024-01-07 18:02:46 +0200

    Fix the array types, and fix typos

--- src/CommandInterface.ts
+++ src/CommandInterface.ts
@@ -7,8 +7,8 @@ export interface Command extends ChatInputApplicationCommandData {
   deferReply?: boolean;
   ownerOnly?: boolean;
   // Permission bits. ex. PermissionBitField.Flags.KickMembers
-  permissions?: [bigint?];
-  // Which servers the command be availabled in (not defining this array will result in it being a global command)
-  servers?: [string];
+  permissions?: bigint[];
+  // Which servers the command should be available in (not defining this array will result in it being a global command)
+  servers?: string[];
   run: (interaction: CommandInteraction) => void;
 }
