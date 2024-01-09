846b24510fe56dbf5d488b6338689fb91c8a4741
Haruka <96925398+nakoyasha@users.noreply.github.com>
2024-01-09 03:41:21 +0200

    Introduce CommandsV2 type

--- src/CommandInterface.ts
+++ src/CommandInterface.ts
@@ -1,14 +1,33 @@
 import {
   CommandInteraction,
   ChatInputApplicationCommandData,
+  SlashCommandBuilder,
+  ApplicationCommandType,
+  ChatInputCommandInteraction,
+  CacheType,
 } from "discord.js";
+export type RunInteraction = CommandInteraction & ChatInputCommandInteraction
 
 export interface Command extends ChatInputApplicationCommandData {
+  // WIP REFACTOR
+  data?: Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">,
   deferReply?: boolean;
   ownerOnly?: boolean;
   // Permission bits. ex. PermissionBitField.Flags.KickMembers
   permissions?: bigint[];
   // Which servers the command should be available in (not defining this array will result in it being a global command)
   servers?: string[];
-  run: (interaction: CommandInteraction) => void;
+  run: (interaction: RunInteraction) => void;
+}
+
+
+export interface CommandV2 {
+  data?: Omit<SlashCommandBuilder, any> & any,
+  deferReply?: boolean;
+  ownerOnly?: boolean;
+  // Permission bits. ex. PermissionBitField.Flags.KickMembers
+  permissions?: bigint[];
+  // Which servers the command should be available in (not defining this array will result in it being a global command)
+  servers?: string[];
+  run: (interaction: RunInteraction) => void;
 }
