5dd7938ce0978e447102d8cdcd4eb4aeb523d164
Haruka <96925398+nakoyasha@users.noreply.github.com>
2024-04-06 01:30:17 +0300

    fix: Use the new DiscordBranch enum instead

--- scripts/ASTExperimentsTest.ts
+++ scripts/ASTExperimentsTest.ts
@@ -1,5 +1,6 @@
 import { ASTPuller } from "@util/Tracker/Fetch/ASTPuller";
+import { DiscordBranch } from "@util/Tracker/Types/DiscordBranch";
 
 (async () => {
-  await new ASTPuller().getClientExperiments("stable")
+  await new ASTPuller().getClientExperiments(DiscordBranch.Stable)
 })()
