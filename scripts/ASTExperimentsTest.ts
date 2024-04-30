b46c7847b169ef82dd8aee5a86d934ac982d99e0
Haruka <96925398+nakoyasha@users.noreply.github.com>
2024-04-30 17:57:18 +0300

    nop

--- scripts/ASTExperimentsTest.ts
+++
@@ -1,6 +0,0 @@
-import { ASTPuller } from "@util/Tracker/Fetch/ASTPuller";
-import { DiscordBranch } from "@util/Tracker/Types/DiscordBranch";
-
-(async () => {
-  await new ASTPuller().getClientExperiments(DiscordBranch.Stable)
-})()
--- scripts/runSaveBuild.ts
+++
@@ -1,8 +0,0 @@
-import { Mizuki } from "@system/Mizuki";
-import { SaveBuild } from "src/Routines/SaveBuild";
-
-(async () => {
-  await Mizuki.init()
-  await Mizuki.start()
-  await new SaveBuild().execute()
-})()
