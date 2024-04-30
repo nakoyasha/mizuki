55164e139c45d5e0f3acd4f775f7179485ad6af5
Haruka <96925398+nakoyasha@users.noreply.github.com>
2024-04-30 03:17:57 +0300

    chore: make eval not do dangerous things

--- src/Commands/Util/eval.ts
+++ src/Commands/Util/eval.ts
@@ -18,21 +18,40 @@ export const Eval: CommandV2 = {
 
     const code = interaction.options.get("code", true)?.value as string
     try {
-      const result = eval(code)
+      // let's break the interaction object! (●'◡'●)
+      const output = []
+      const filteredCode = code
+        .replaceAll("interaction", "null")
+        .replaceAll("console.log", "output.push")
+        .replaceAll("console.warn", "output.push")
+        .replaceAll("console.error", "output.push")
+        .replaceAll("TOKEN", "meow")
+      const result = eval(filteredCode)
 
       if (result === undefined || result === "") {
         await interaction.followUp(constants.messages.EVAL_CODE_RAN_NO_OUTPUT)
         return;
       }
 
-      await interaction.followUp(result)
+      // incase someone still somehow gets the token
+      if (result === process.env.TOKEN) {
+        await interaction.followUp(
+          constants.messages
+            .EVAL_CODE_USER_TRIED_TO_SEND_THE_BOT_TOKEN_BUT_WE_WONT_LET_THEM_DO_THAT_BECAUSE_OH_GOD_THAT_IS_BAD_VERY_VERY_VERY_BAD
+        )
+        return;
+      }
+
+      // safe tostring (hopefully??)
+      // NOTE: make it actually safe if it isn't
+      await interaction.followUp(`${result}`)
     } catch (err: any) {
       if (err.message.includes("Cannot send an empty message")) {
         await interaction.followUp(constants.messages.EVAL_CODE_RAN_NO_OUTPUT)
         return;
       }
 
-      await interaction.followUp(`Exception while running code: \`${err}\``)
+      await interaction.followUp(`Exception while running code: \`\`\`diff\n-${err}\n\`\`\` `)
     }
   }
 }
