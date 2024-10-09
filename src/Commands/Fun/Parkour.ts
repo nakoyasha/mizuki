f2510c0a29e0a5840ddf2a89677efe72c54995f7
Haruka <personal@shiroko.me>
2024-10-09 16:32:23 +0300

    revert: revert parkour to pre-subcommands, im too lazy to debug discord.js's terrible error messages

--- src/Commands/Fun/Parkour.ts
+++ src/Commands/Fun/Parkour.ts
@@ -1,10 +1,5 @@
 // In Parkour Civilizatio-
-import {
-  SlashCommandBuilder,
-  CommandInteraction,
-  codeBlock,
-  ChatInputCommandInteraction,
-} from "discord.js";
+import { SlashCommandBuilder, CommandInteraction, codeBlock } from "discord.js";
 import { CommandV2 } from "src/CommandInterface";
 
 // Phew, thank god it's Latin!
@@ -50,59 +45,30 @@ export const ParkourTranslate: CommandV2 = {
   data: new SlashCommandBuilder()
     .setName("parkour")
     .setDescription(
-      "Translates text into the Parkour Civilization's language and back. (why did i make this)"
+      "Translates text into the Parkour Civilization's language. (why did i make this)"
     )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName("translate-to")
-        .addStringOption((option) =>
-          option
-            .setName("text")
-            .setDescription("What do you want to translate?")
-            .setRequired(true)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName("translate-from")
-        .addStringOption((option) =>
-          option
-            .setName("text")
-            .setDescription("What do you want to translate?")
-            .setRequired(true)
-        )
+    .addStringOption((option) =>
+      option
+        .setName("text")
+        .setDescription("What do you want to translate?")
+        .setRequired(true)
     ),
-
-  run: async (interaction: ChatInputCommandInteraction) => {
+  run: async (interaction: CommandInteraction) => {
     const text = interaction.options.get("text")?.value as string;
-
     const split = text.split("");
+
     let result = "";
 
-    if (interaction.options.getSubcommand() == "translate-to") {
-      for (let character of split) {
-        const lowerCase = character.toLowerCase();
-        const parkourLetter = languageMap.get(lowerCase);
+    for (let character of split) {
+      const lowerCase = character.toLowerCase();
+      const parkourLetter = languageMap.get(lowerCase);
 
-        if (parkourLetter == undefined) {
-          console.warn(`Unknown letter: ${lowerCase}`);
-          continue;
-        }
-
-        result = `${result}${parkourLetter}`;
+      if (parkourLetter == undefined) {
+        console.warn(`Unknown letter: ${lowerCase}`);
+        continue;
       }
-    } else if (interaction.options.getSubcommand() == "translate-from") {
-      for (let character of split) {
-        const lowerCase = character.toLowerCase();
-        const parkourLetter = languageMap.get(lowerCase);
 
-        if (parkourLetter == undefined) {
-          console.warn(`Unknown letter: ${lowerCase}`);
-          continue;
-        }
-
-        result = `${result}${parkourLetter}`;
-      }
+      result = `${result}${parkourLetter}`;
     }
 
     await interaction.reply(codeBlock(result));
